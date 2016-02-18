cd1b9d3872e9ad83ad5c46870d1b4b98b6b6ec42
prorockband <prorockband@aol.com>
2016-02-17 21:01:10 -0500

    Shark week

---
+++ HoFGameJam/Assets/Scripts/SimplePlayer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimplePlayer : MonoBehaviour {
+
+    public GameObject player;
+    public float Speed = 0f;
+    private float movex = 0f;
+    private float movey = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            this.GetComponent<SpriteRenderer>().flipX = false;
+            position.x--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            this.GetComponent<SpriteRenderer>().flipX = true;
+            position.x++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y--;
+            this.transform.position = position;
+        }
+    }
+}
