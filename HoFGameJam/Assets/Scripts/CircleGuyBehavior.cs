5ab3253cafa23655a84750c28c11feefa616a1a7
LamentedHarp <longjof@woh.rr.com>
2016-02-18 10:48:39 -0500

    Gave the orange angler some goofy movement, prepped for item pickup.

---
+++ HoFGameJam/Assets/Scripts/CircleGuyBehavior.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleGuyBehavior : MonoBehaviour {
+
+    bool up_or_down;
+    int count = 0;
+    int spiral_radius = 20;
+    float delay_timer = 2.5f;
+
+    Vector3 target_position;
+
+    // Use this for initialization
+    void Start () {
+        up_or_down = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (target_position != new Vector3(0.0f, 0.0f, 0.0f))
+        {
+            delay_timer -= Time.deltaTime;
+
+            if (delay_timer < 0.0f)
+            {
+                // spinning corkscrew
+                if (up_or_down)
+                {
+                    if (transform.position.x < target_position.x)
+                    {
+                        transform.position = new Vector3(transform.position.x + .1f, transform.position.y + .1f, transform.position.z);
+                    }
+                    else transform.position = new Vector3(transform.position.x - .1f, transform.position.y + .1f, transform.position.z);
+                    if (count == spiral_radius)
+                        up_or_down = false;
+                }
+
+                else if (!up_or_down)
+                {
+                    if (transform.position.x < target_position.x)
+                    {
+                        transform.position = new Vector3(transform.position.x + .1f, transform.position.y - .1f, transform.position.z);
+                    }
+                    else transform.position = new Vector3(transform.position.x - .1f, transform.position.y - .1f, transform.position.z);
+                    if (count == spiral_radius)
+                        up_or_down = true;
+                }
+                count++;
+
+                if (count > spiral_radius) GetComponent<SpriteRenderer>().flipY = !GetComponent<SpriteRenderer>().flipY;
+
+
+                if (transform.position.x < target_position.x)
+                {
+                    GetComponent<SpriteRenderer>().flipX = true;
+                }
+                else
+                {
+                    GetComponent<SpriteRenderer>().flipX = false;
+                }
+                Vector3 vectorToTarget = target_position - transform.position;
+                float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+                if (GetComponent<SpriteRenderer>().flipX == false)
+                {
+                    angle = -angle;
+                }
+                Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+                transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 5);
+
+                if (count > spiral_radius)
+                {
+                    count = 0;
+                    spiral_radius -= 3;
+                }
+
+                if (spiral_radius <= 0)
+                {
+                    spiral_radius = 20;
+                    target_position = new Vector3(0.0f, 0.0f, 0.0f);
+                    delay_timer = 2.5f;
+                }
+            }
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            target_position = coll.GetComponent<Transform>().position;
+        }
+    }
+}
---
+++ HoFGameJam/Assets/Scripts/Enemy.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+    public GameObject enemy, point1, point2;
+    public float movementSpeed, step, startSpeed;
+
+    public bool isTouching = false;
+
+    private bool goToPoint1 = true;
+
+    public float timeRemaining = 5;
+
+    private Vector3 location;
+
+    // Use this for initialization
+    void Start () {
+        enemy.transform.position = (point1.transform.position + point2.transform.position) * 0.5f;
+        startSpeed = movementSpeed;
+        enemy.tag = "Enemy";
+
+        step = movementSpeed * Time.deltaTime;
+
+        if (movementSpeed < 0)
+        {
+            movementSpeed = 0;
+        }
+
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        step = movementSpeed * Time.deltaTime;
+
+        if (!isTouching)
+        {
+
+
+            if (goToPoint1)
+            {
+                enemy.transform.position = Vector3.MoveTowards(transform.position, point1.transform.position, step);
+                enemy.transform.rotation = Quaternion.Euler(0, 0, 0);
+                enemy.GetComponent<SpriteRenderer>().flipX = false;
+                if (point1.transform.position == enemy.transform.position)
+                {
+                    goToPoint1 = false;
+                }
+            }
+            else
+            {
+                enemy.transform.position = Vector3.MoveTowards(transform.position, point2.transform.position, step);
+                enemy.transform.rotation = Quaternion.Euler(0, 0, 0);
+                enemy.GetComponent<SpriteRenderer>().flipX = true;
+                if (point2.transform.position == enemy.transform.position)
+                {
+                    goToPoint1 = true;
+                }
+
+            }
+        }
+        if (isTouching)
+        {
+            timeRemaining -= Time.deltaTime;
+            if (timeRemaining < 0 && timeRemaining > -3)
+            {
+                movementSpeed = 5;
+                step = movementSpeed * Time.deltaTime;               
+
+                enemy.transform.position = Vector3.MoveTowards(transform.position, location, step);
+            }
+
+           
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (enemy.transform.position.x < location.x)
+        {
+            enemy.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            enemy.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        Vector3 vectorToTarget = location - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        if (enemy.GetComponent<SpriteRenderer>().flipX == false)
+        {
+            angle = -angle;
+        }
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 5);
+
+        location = coll.GetComponent<Transform>().transform.position;
+
+        isTouching = true;
+    }
+    void OnTriggerStay2D(Collider2D coll)
+    {
+        if (timeRemaining < -3)
+        {
+            location = coll.GetComponent<Transform>().transform.position;
+            timeRemaining = 5;
+        }
+        isTouching = true;
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        timeRemaining = 5;
+        movementSpeed = startSpeed;
+        isTouching = false;
+    }
+
+}
