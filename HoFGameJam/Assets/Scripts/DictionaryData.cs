7bdda28b761e0aa4f95d0ae57a7be302488aa184
LamentedHarp <longjof@woh.rr.com>
2016-02-17 20:49:49 -0500

    Level 1 started and stuff.

---
+++ HoFGameJam/Assets/Scripts/DictionaryData.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+public class DictionaryData : MonoBehaviour
+{
+    string filename;
+
+    public string[] three_letter_words;
+    public string[] four_letter_words;
+    public string[] seven_letter_words;
+
+    // Use this for initialization
+    void Start()
+    {
+        filename = "Assets\\GJ_Dictionary.txt";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public bool Load()
+    {
+        string line;
+        StreamReader theReader = new StreamReader(filename, Encoding.Default);
+        for (int i = 0; i < 3; ++i)
+        { 
+            line = theReader.ReadLine();
+
+            int num_words = int.Parse(line);
+
+            line = theReader.ReadLine();
+
+            int word_length = int.Parse(line);
+
+            if (word_length == 3) three_letter_words = new string[num_words];
+            else if (word_length == 4) four_letter_words = new string[num_words];
+            else if (word_length == 7) seven_letter_words = new string[num_words];
+
+            for (int j = 0; j < num_words; ++j)
+            {
+                line = theReader.ReadLine();
+
+                if (word_length == 3) three_letter_words[j] = line;
+                else if (word_length == 4) four_letter_words[j] = line;
+                else if (word_length == 7) seven_letter_words[j] = line;
+            }
+        }
+
+        theReader.Close();
+
+        return true;
+
+    }
+}
+
---
+++ HoFGameJam/Assets/Scripts/WordManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WordManager : MonoBehaviour {
+
+    public GameObject[] letter_spawners;
+    public string three_letter_word;
+    public string four_letter_word;
+    public string seven_letter_word;
+    int[] used_numbers = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+
+    // Use this for initialization
+    void Start () {
+        letter_spawners = GameObject.FindGameObjectsWithTag("Letter");
+
+        bool result = GetComponent<DictionaryData>().Load();
+        if (result)
+        {
+            three_letter_word = GetComponent<DictionaryData>().three_letter_words
+                [Random.Range(0, GetComponent<DictionaryData>().three_letter_words.Length)];
+            four_letter_word = GetComponent<DictionaryData>().four_letter_words
+                [Random.Range(0, GetComponent<DictionaryData>().four_letter_words.Length)];
+            seven_letter_word = GetComponent<DictionaryData>().seven_letter_words
+                [Random.Range(0, GetComponent<DictionaryData>().seven_letter_words.Length)];
+
+            for (int i = 0; i < 3; ++i)
+            {
+                int number = Random.Range(0, letter_spawners.Length);
+                while (number == used_numbers[0] || number == used_numbers[1] || number == used_numbers[2])
+                {
+                    number++;
+                    if (number >= letter_spawners.Length)
+                        number = 0;
+                }
+                used_numbers[i] = number;
+                char letter = three_letter_word[i];
+                letter_spawners[number].GetComponent<LetterSpawn>().InstantiateLetter(letter);
+            }
+            for (int i = 0; i < 4; ++i)
+            {
+                int number = Random.Range(0, letter_spawners.Length);
+                while (number == used_numbers[0] || number == used_numbers[1] || number == used_numbers[2] ||
+                    number == used_numbers[3] || number == used_numbers[4] || number == used_numbers[5] || number == used_numbers[6])
+                {
+                    number++;
+                    if (number >= letter_spawners.Length)
+                        number = 0;
+                }
+                used_numbers[i + 3] = number;
+                char letter = four_letter_word[i];
+                letter_spawners[number].GetComponent<LetterSpawn>().InstantiateLetter(letter);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    for (int i = 0; i < 7; ++i)
+        {
+            if (i < 3)
+                letter_spawners[used_numbers[i]].GetComponent<LetterSpawn>().InstantiateLetter(three_letter_word[i]);
+            else
+                letter_spawners[used_numbers[i]].GetComponent<LetterSpawn>().InstantiateLetter(four_letter_word[i - 3]);
+        }
+	}
+}
